26338210128cdc8afc0e8f632a5c404516c5a8de
José Manuel Nieto <superjmn@outlook.com>
2023-07-20 12:32:48 +0200

    Add project files.

---
+++ ToggleSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace ToggleSample.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public IList<Secret> Secrets { get; } = new List<Secret> { new("Dog", true), new("Cat", false) };
+}
+
+public class Secret : ViewModelBase
+{
+    private bool confirmed;
+
+    public Secret(string text, bool willBeCorrectOnConfirmation)
+    {
+        Text = text;
+        IsCorrect = this.WhenAnyValue(x => x.Confirmed, isConfirmed => isConfirmed && willBeCorrectOnConfirmation);
+        IsWrong = this.WhenAnyValue(x => x.Confirmed, isConfirmed => isConfirmed && !willBeCorrectOnConfirmation);
+    }
+
+    public IObservable<bool> IsWrong { get; }
+
+    public string Text { get; }
+
+    [Reactive]
+    public bool Confirmed { get; set; }
+
+    public IObservable<bool> IsCorrect { get; }
+}
---
+++ ToggleSample/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ToggleSample.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
